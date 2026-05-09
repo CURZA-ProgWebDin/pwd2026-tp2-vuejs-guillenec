98e7ae079afd019956fc9af7bfaf3f3b7eba0ec2
guillenec <guillermoneculqueo@gmail.com>
2026-05-08 23:12:57 -0300

    feat(component): add ProductList

--- gestorProductos_vue/src/App.vue
+++ gestorProductos_vue/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue";
-import FormularioProducros from "./components/FormularioProducros.vue";
-
+import ProductForm from "./components/ProductForm.vue";
+import ProductList from "./components/ProductList.vue";
 const productos = ref([]);
 
 const crearProducto = (producto) => {
@@ -20,11 +20,12 @@ const listarProductos = (e) => {
 </script>
 
 <template>
-  <FormularioProducros @crear-producto="crearProducto" />
+  <ProductForm @crear-producto="crearProducto" />
   <button
     class="p-2 rounded-md bg-orange-500 text-white"
     @click="listarProductos"
   >
     ver lista
   </button>
+  <ProductList :productos="productos" />
 </template>
---
+++ gestorProductos_vue/src/components/ProductList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  productos: { type: Array, required: true },
+});
+</script>
+
+<template>
+  <div class="product-list">
+    <h2>Lista de Productos</h2>
+    <ul>
+      <li v-for="producto in productos" :key="producto.id">
+        {{ producto.nombre }} - ${{ producto.precio }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang=""></style>
